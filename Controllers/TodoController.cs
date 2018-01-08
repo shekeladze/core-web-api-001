2efb9e63c1d21a8ca4dc7d14bdadce7600e33116
shshekeladze <shshekeladze@SSHEKELADZE>
2018-01-08 17:24:54 +0400

    new task 001

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -19,6 +19,7 @@ namespace TodoApi.Controllers
                 _context.TodoItems.Add(new TodoItem { Name = "Buy milk" });
                 _context.TodoItems.Add(new TodoItem { Name = "Wash car" });
                 _context.TodoItems.Add(new TodoItem { Name = "Buy book" });
+                _context.TodoItems.Add(new TodoItem { Name = "Investigate docker test best practicies" });
                 _context.SaveChanges();
             }
         }
